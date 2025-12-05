4869a40f7ee25e5822b2fc78cb2dab972b6a5ba6
Ella2313 <joyfullgabi@gmail.com>
2025-12-05 21:21:41 +0300

    uhhhh rechecking my code

--- js/Scripts.js
+++ js/Scripts.js
@@ -11,28 +11,28 @@ window.addEventListener('scroll', () => {
 const navbarCollapseDiv = document.getElementById('navbar-collapse');
 const navbarShowBtn = document.getElementById('navbar-show-btn');
 const navbarCloseBtn = document.getElementById('navbar-close-btn');
-
+// show navbar
 navbarShowBtn.addEventListener('click', () => {
     navbarCollapseDiv.classList.add('navbar-collapse-rmw');
 });
 
-navbarCloseBtn.addEventListener('click',() => {
-    navbarCollapseDive.classList.remove('navbar-collapse-rmw');
+// hide side bar
+navbarCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
 });
 
 document.addEventListener('click', (e) => {
-    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn"){
+    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn"){
         navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
     }
 });
 
-
+// stop transition and animatino during window resizing
 let resizeTimer;
-window.addEventLintener('resize', () => {
-    document.body.classList.add('resize-animation-stopper');
+window.addEventListener('resize', () => {
+    document.body.classList.add("resize-animation-stopper");
     clearTimeout(resizeTimer);
-    resizeTime = setTimerout(() => {
-        document.body.classList.remove('resize-animation-stopper');
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
     }, 400);
 });
-
